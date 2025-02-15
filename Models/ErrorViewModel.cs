16cf9322b4af8cc39cc3893b8cc40d239116502b
Jiysea <jiyseacampos@gmail.com>
2025-02-15 22:52:40 +0800

    v0.1.0 - Initialization of the project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Schedify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
