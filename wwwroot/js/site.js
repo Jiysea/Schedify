44fb16b4142a33377177b882b3e605d613226904
Jiysea <jiyseacampos@gmail.com>
2025-02-18 18:14:04 +0800

    v0.1.0.8 - Flatpickr addition

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// const flatpickr = require("flatpickr");
+
+// es modules are recommended, if available, especially for typescript
+import flatpickr from "flatpickr";
